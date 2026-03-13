95f378592fe1f1a9a3798ffd31c0c864fab49409
Kamryn Wagner <wkamryn26@gmail.com>
2026-03-13 05:33:20 +0000

    Implemented FCFS

--- process_control_block.c
+++ process_control_block.c
@@ -8,7 +8,7 @@ typedef struct PCB {
     int BURST_TIME;
     int REM_TIME;
     int PRIORITY;
-    char STATE[12];
+    char STATE[12]; //NEW, READY, RUNNING, or TERMINATED
     int CPU_START;
     int CPU_END;
 } PCB;
@@ -27,8 +27,67 @@ PCB* create_process(char* PID, int ARR_TIME, int BURST_TIME, int PRIORITY) {
     return new_process;
 }
 
+
 void print_process(PCB* process) {
     printf("PID: %s, Arr: %d, Burst: %d, Rem %d, Prio %d, State: %s\n", process->PID, process->ARR_TIME, process->BURST_TIME, process->REM_TIME, process->PRIORITY, process->STATE);
+}
+
+void update_process(PCB* process, char* new_state) {
+    strcpy(process->STATE, new_state);
+    process->CPU_END++;
+    print_process(process);
+}
+
+void increment_time(PCB* process) {
+    if (strcmp(process->STATE, "RUNNING") != 0) return;
+
+    process->REM_TIME--;
+
+    if (process->REM_TIME == 0) {
+        update_process(process, "TERMINATED");
+    } 
+}
+
+void first_come_first_serve(PCB* processes[], int proc_count, int time) {
+    int process_running = 1;
+    for (int proc_index; proc_index < proc_count; proc_index++) {
+        if (strcmp(processes[proc_index]->STATE, "RUNNING") == 0) {
+            process_running = 0;
+            break;
+        }
+    }
+
+    if (process_running) {
+        for (int proc_index; proc_index < proc_count; proc_index++) {
+            PCB* current_proc = processes[proc_index];
+            if (strcmp(current_proc->STATE, "READY") == 0) {
+                update_process(current_proc, "RUNNING");
+                break;
+            }
+        }
+    }
+}
+
+void begin_clock(PCB* processes[], int proc_count, int total_time) {
+    for (int time = 0; time < total_time; time++) {
+        printf("\nTime: %d\n", time);
+        for (int proc_index = 0; proc_index < proc_count; proc_index++) {
+            PCB* current_proc = processes[proc_index]; 
+            if (current_proc->ARR_TIME == time && strcmp(current_proc->STATE, "NEW") == 0) {
+                update_process(current_proc, "READY");
+            }
+            increment_time(current_proc);
+        }
+        
+        first_come_first_serve(processes, proc_count, time);
+
+        for (int proc_index = 0; proc_index < proc_count; proc_index++) {
+            PCB* current_proc = processes[proc_index];
+            if (strcmp(current_proc->STATE, "NEW") != 0 && strcmp(current_proc->STATE, "TERMINATED") != 0) {
+                increment_time(current_proc);
+            }
+        }
+    }
 }
 
 int main() {
@@ -38,6 +97,7 @@ int main() {
     char PID[12];
     int ARR_TIME, BURST_TIME, PRIORITY;
     int index = 0;
+    
 
     char line_buffer[1024];
     while (fgets(line_buffer, 1024, file) != NULL) {
@@ -46,7 +106,12 @@ int main() {
        }
     }
 
-    for (int i = 0; i < sizeof(processes) / sizeof(processes[0]); i++) {
+    int proc_count = sizeof(processes) / sizeof(processes[0]);
+    for (int i = 0; i < proc_count; i++) {
         print_process(processes[i]);
     }
+    fclose(file);
+
+    begin_clock(processes, proc_count, 12);
+    return 0;
 }
